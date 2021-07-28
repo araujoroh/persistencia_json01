00e6e19713ee11e670f3fd2ade97833428fb994b
Robson <robsonrs.ra@gmail.com>
2021-07-27 22:23:44 -0300

    Leitor de Arquivo Json par 2

--- src/main/java/util/LeitorJson.java
+++ src/main/java/util/LeitorJson.java
@@ -2,6 +2,8 @@ package util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,22 +14,31 @@ public class LeitorJson {
 
 	static Map<String, String> mapJson = new HashMap<String, String>();
 	static Gson gson = new Gson();
-	
 	static Reader reader;
 	
-	
-	
-	public static void main(String[] args) throws FileNotFoundException {
+		
+	public static void main(String[] args) throws IOException {
 	reader = new FileReader("./src/main/resources/MassaTeste.json");
 	mapJson = gson.fromJson(reader, Map.class);
 	
 	System.out.println(mapJson.get("url"));
 	System.out.println(mapJson.get("produto"));
 	System.out.println(mapJson.get("valor"));
+	gravaArquivoJson();
 	
-	
-		// TODO Auto-generated method stub
-
 	}
 
+	private static void gravaArquivoJson() throws IOException {
+		mapJson.put("url", "http://qaacademy.com.br");
+		mapJson.put("curso", "testes automatizados de api");
+		mapJson.put("endereço", "Av Paulista, 509");
+		
+		FileWriter writer = new FileWriter("./src/main/resources/massaTeste_writer.json");
+		gson.toJson(mapJson, writer); 
+		writer.flush();
+		
+	
+		
+		
+	}
 }
